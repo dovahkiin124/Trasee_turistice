c16f917119482dcb936cc75416b9798489e2652e
Ghidoarca Petru_Cristi <petru.ghidoarca@student.usv.ro>
2023-05-02 11:20:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -87,105 +87,127 @@ int main() {
     do{
         AgentieTurism agentie;
         vector<Booking> bookings;
+
         switch(Optiune)
         {
             case 1: {
+                char parola[10];
+                const char *parol = "Game"; //Parola admin tip default
+                cout << "Introduceti parola admin: ";
+                int nr_incercari = 0; // initializare numarator pentru incercarile de introducere a parolei incorecte
+                while(nr_incercari < 3) // accepta maxim 3 incercari incorecte
+                {
+                    cin >> parola;
+                    if (strcmp(parol,parola) == 0) //Verificare parola
+                    {
+                        Meniu_administrator admin;
+                        int optiune;
+                        do {
+                            admin.afiseaza();
+                            cin >> optiune;
+                            cout << endl;
 
+                            switch (optiune) {
+                                case 0:
+                                    exit(1);
+                                case 1: {
+                                    string nume, des;
+                                    int pret;
 
-                Meniu_administrator admin;
-                int optiune;
-                do {
-                    admin.afiseaza();
-                    cin >> optiune;
-                    cout << endl;
+                                    cout << "Introduceti numele pachetului turistic: ";
+                                    cin >> nume;
+                                    cout << "Introduceti descrierea pachetului turistic: ";
+                                    cin >> des;
+                                    cout << "Introduceti pretul pachetului turistic: ";
+                                    cin >> pret;
 
-                    switch (optiune) {
-                        case 0:
-                            exit(1);
-                        case 1: {
-                            string nume, des;
-                            int pret;
-
-                            cout << "Introduceti numele pachetului turistic: ";
-                            cin >> nume;
-                            cout << "Introduceti descrierea pachetului turistic: ";
-                            cin >> des;
-                            cout << "Introduceti pretul pachetului turistic: ";
-                            cin >> pret;
-
-                            PachetTuristic pachet(nume, des, pret);
-                            agentie.adaugaPachetTuristic(pachet);
-                            cout << "Pachetul turistic a fost adaugat cu succes!" << std::endl << std::endl;
-                            break;
-                        }
-                        case 2:
-                            agentie.afiseazaPachete();
-                            break;
-                        case 3: {
-
-                            string nume, prenume;
-                            cout << "Introduceti numele turistului: " << endl;
-                            cin >> nume;
-                            cout << "Introduceti prenumele turistului: " << endl;
-                            cin >> prenume;
-
-
-                            Turist turist(nume, prenume);
-                            agentie.adaugaTurist(turist);
-
-
-
-                            break;
-                        }
-                        case 4: {
-                            int  turistId;
-
-
-                            cout << "Introduceti ID-ul turistului pe care doriti sa il stergeti: ";
-                            cin >> turistId;
-                            agentie.stergeTurist(turistId);
-                            break;
-
-
-
-                        }
-                        case 5:
-                            if (bookings.empty()) {
-                                cout << "Nici o rezervare inca." << endl;
-                            } else {
-                                cout << "Rezervari:" << endl;
-                                for (const auto& booking : bookings) {
-                                    cout << "Oferta: " << booking.getOffer().getName() << endl;
-                                    cout << "Cantitate: " << booking.getQuantity() << endl;
-                                    cout << "Pret total: " << booking.getTotalPrice() << endl << endl;
+                                    PachetTuristic pachet(nume, des, pret);
+                                    agentie.adaugaPachetTuristic(pachet);
+                                    cout << "Pachetul turistic a fost adaugat cu succes!" << std::endl << std::endl;
+                                    break;
                                 }
-                            }
+                                case 2:
+                                    agentie.afiseazaPachete();
+                                    break;
+                                case 3: {
 
-                            break;
-                        case 6:
-                        {
-                            agentie.afiseazaTuristi();
-                            break;
-                        }
-                        case 7:
-                        {
-                            int id;
-                            cout << "Introduceii ID-ul pachetului de sters: ";
-                            cin >> id;
-                            agentie.stergePachetTuristic(id);
-                            break;
-                        }
-                        default:
-                            cout << "Optiune invalida! Va rugam sa introduceti o optiune valida." << std::endl
-                                 << std::endl;
+                                    string nume, prenume;
+                                    cout << "Introduceti numele turistului: " << endl;
+                                    cin >> nume;
+                                    cout << "Introduceti prenumele turistului: " << endl;
+                                    cin >> prenume;
+
+
+                                    Turist turist(nume, prenume);
+                                    agentie.adaugaTurist(turist);
+
+
+
+                                    break;
+                                }
+                                case 4: {
+                                    int  turistId;
+
+
+                                    cout << "Introduceti ID-ul turistului pe care doriti sa il stergeti: ";
+                                    cin >> turistId;
+                                    agentie.stergeTurist(turistId);
+                                    break;
+
+
+
+                                }
+                                case 5:
+                                    if (bookings.empty()) {
+                                        cout << "Nici o rezervare inca." << endl;
+                                    } else {
+                                        cout << "Rezervari:" << endl;
+                                        for (const auto& booking : bookings) {
+                                            cout << "Oferta: " << booking.getOffer().getName() << endl;
+                                            cout << "Cantitate: " << booking.getQuantity() << endl;
+                                            cout << "Pret total: " << booking.getTotalPrice() << endl << endl;
+                                        }
+                                    }
+
+                                    break;
+                                case 6:
+                                {
+                                    agentie.afiseazaTuristi();
+                                    break;
+                                }
+                                case 7:
+                                {
+                                    int id;
+                                    cout << "Introduceii ID-ul pachetului de sters: ";
+                                    cin >> id;
+                                    agentie.stergePachetTuristic(id);
+                                    break;
+                                }
+                                default:
+                                    cout << "Optiune invalida! Va rugam sa introduceti o optiune valida." << std::endl
+                                         << std::endl;
+                            }
+                        } while (optiune != 0);
                     }
-                } while (optiune != 0);
+                    else
+                    {
+                        nr_incercari++; // incrementeaza numarul de accesari incorecte
+                        cout << "Parola incorecta\nIncercati din nou\n";
+                    }
+                }
+                if(nr_incercari >= 3) // daca sunt facute 3incercari incorecte programul se va inchide
+                {
+                    cout << "Prea multe incercari incorecte. Programul se inchide.";
+                    exit(0); //inchidere program
+                }
                 break;
             }
             case 2:
             {
                 cout << "1. Logare" << endl;
                 cout << "2. Inregistrare" << endl;
+
+
                 int choice;
                 cin >> choice;
 
